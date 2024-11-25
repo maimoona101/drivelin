2dba4e625d50cd22bf583fca0ca3669ea0dfb6ed
mansoor1123456 <164479871+maimoona123456@users.noreply.github.com>
2024-11-25 22:15:05 +0500

    erroe resolve

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+//import Image from "next/image";
+//import Link from "next/link";
 import Navbar from "./navbar/page";
 import HeroPage from "./home/page";
-import Contact from "./contact/page";
-import About from "./about/page";
+//import Contact from "./contact/page";
+//import About from "./about/page";
 
 // Thank you for taking the time to visit my website!
 
